41c8381fdb959f2e30314121d2e1b196fa854678
mggbhn <maggiebswim@gmail.com>
2018-07-11 15:51:09 +0000

    automatically backed up by learn

--- currency.rb
+++ currency.rb
@@ -1,6 +1,6 @@
-def usd_to_eur(x=0)
+def usd_to_eur(x)
   eur_to_usd = []
-  dollars = [1,2,3]
+  dollars = [x]
   dollars.each do |x|
    eur_to_us = 1.20*x
    eur_to_usd << eur_to_us
@@ -9,8 +9,13 @@ end
 
 puts usd_to_eur(3)
 
-def eur_to_usd
-  
-end
+def eur_to_usd(x)
+  usd_to_eur = []
+  euros = [x]
+  euros.each do |x|
+   usd_to_eu = 1.20*x
+   usd_to_eur << usd_to_eu
+ end
+end 
 
 
